aadc1107373e5eef013ca402e0f0659d3f87622d
Amos Gichero <amosgichero@gmail.com>
2020-05-26 15:18:55 -0400

    api wired

--- src/App.js
+++ src/App.js
@@ -4,13 +4,24 @@ import Cards from './components/Cards/Cards';
 import Chart from './components/Chart/Chart';
 import CountryPicker from './components/CountryPicker/CountryPicker';
 
-import styles from './App.css'
+import styles from './App.css';
+import {fetchData} from './api/config';
 
 class App extends Component{
+    state = {
+        data: {}
+    }
+
+    async componentDidMount(){
+        const fetchedData = await fetchData();
+        this.setState({data: fetchedData})
+    }
     render(){
+        const {data} = this.state;
+
         return(
             <div className={styles.container}>
-                <Cards />
+                <Cards data={data}/>
                 <Chart />
                 <CountryPicker />
             </div>
--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,9 +1,57 @@
 import React from 'react';
+import styles from './Cards.css';
+import {Card, CardContent, Typography, Grid} from '@material-ui/core';
+import CountUp from 'react-countup';
 
-const Cards = () => {
+const Cards = ({data: {confirmed, recovered, deaths, lastUpdate}}) => {
+    console.log(confirmed)
+    if(!confirmed){
+        return 'Loading...'
+    }
     return(
-        
-            <h1>Cards</h1>
+        <div className={styles.cardContainer}>
+            <Grid container spacing={3} justify="center">
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Infected</Typography>
+                        <Typography variant="h5">
+                            <CountUp
+                            start={0}
+                            end={confirmed.value}
+                            />
+                        </Typography>
+                        <Typography color="textSecondary">Real Date</Typography>
+                        <Typography variant="body2">Number of active Covid-19 cases</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+                        <Typography variant="h5">
+                            <CountUp
+                                start={0}
+                                end={recovered.value}
+                            />
+                        </Typography>
+                        <Typography color="textSecondary">Real Date</Typography>
+                        <Typography variant="body2">Number of recoveries from Covid-19</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Deaths</Typography>
+                        <Typography variant="h5">
+                            <CountUp
+                                start={0}
+                                end={deaths.value}
+                            />
+                        </Typography>
+                        <Typography color="textSecondary">Real Date</Typography>
+                        <Typography variant="body2">Number of deaths from Covid-19</Typography>
+                    </CardContent>
+                </Grid>
+            </Grid>
+        </div>
         
     )
 }
